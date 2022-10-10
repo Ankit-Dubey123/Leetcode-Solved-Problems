13d7f6ecfcf82636c23165294505435becbd8d3a
Ankit Dubey <97017655+Ankit-Dubey123@users.noreply.github.com>
2022-10-10 17:52:30 +0530

    Time: 28 ms (57.33%), Space: 11 MB (32.75%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,8 +3,6 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int>res;
         unordered_map<int,int>mp;
-        
-        
         for(int i=0;i<nums.size();i++){
             if(mp.find(target-nums[i])!=mp.end()){
                 res.push_back(i);
