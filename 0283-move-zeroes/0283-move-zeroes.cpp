894b8f83163814a9e68ba07df73e675215ef646b
Ankit Dubey <97017655+Ankit-Dubey123@users.noreply.github.com>
2022-11-19 11:37:19 +0530

    Time: 23 ms (94.32%), Space: 19.3 MB (15.00%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,7 +3,7 @@ public:
     void moveZeroes(vector<int>& nums) {
         int i=0;
         int j=1;
-        while(i<nums.size() and j<nums.size()){
+        while(j<nums.size()){
             if(nums[i]==0 and nums[j]==0){
                 j++;
             }
