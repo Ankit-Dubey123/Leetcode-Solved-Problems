72eabe202e06c6b986a0ab90205677579d6b9b78
Ankit Dubey <97017655+Ankit-Dubey123@users.noreply.github.com>
2022-11-28 11:00:57 +0530

    Time: 86 ms (16.95%), Space: 22.3 MB (55.97%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,26 +2,39 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         // one option is to XOR all elements
-        //2nd-----------
-        int i=0;
-        int j=nums.size()-1;
-        if(j==0)return nums[0];
-        while(i<=j){
-            if(nums[i]==nums[i+1]){
-                i++;
+        //-----------------2nd-----------
+        // int i=0;
+        // int j=nums.size()-1;
+        // if(j==0)return nums[0];
+        // while(i<=j){
+        //     if(nums[i]==nums[i+1]){
+        //         i++;
+        //     }
+        //     else{
+        //         return nums[i];
+        //     }
+        //     if(nums[j]==nums[j-1]){
+        //         j--;
+        //     }
+        //     else{
+        //         return nums[j];
+        //     }
+        //     i++;
+        //     j--;
+        // }
+        // return -1;
+        //-------------3rd-------------------
+         int l=0;
+        int h=nums.size()-2;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(nums[mid]==nums[mid^1]){
+                l=mid+1;
             }
             else{
-                return nums[i];
+                h=mid-1;
             }
-            if(nums[j]==nums[j-1]){
-                j--;
-            }
-            else{
-                return nums[j];
-            }
-            i++;
-            j--;
         }
-        return -1;
+        return nums[l];
     }
 };
