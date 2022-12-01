6d81baf3b5d9ed1e0701551027600f9a001ac2c4
Ankit Dubey <97017655+Ankit-Dubey123@users.noreply.github.com>
2022-12-01 12:13:50 +0530

    Time: 14 ms (82.42%), Space: 8.1 MB (58.22%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,14 +9,26 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_set<ListNode*>st;
-        while(head!=NULL){
-            if(st.find(head)!=st.end()){
+        // unordered_set<ListNode*>st;
+        // while(head!=NULL){
+        //     if(st.find(head)!=st.end()){
+        //         return true;
+        //     }
+        //     st.insert(head);
+        //     head=head->next;
+        // }
+        // return false;
+        //--------------------------2nd method tortoise method
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast and fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
                 return true;
             }
-            st.insert(head);
-            head=head->next;
         }
         return false;
+        
     }
 };
