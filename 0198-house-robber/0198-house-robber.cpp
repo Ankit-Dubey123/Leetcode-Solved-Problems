e9480b8c637b119169e07090d13488ce1c473e74
Ankit Dubey <97017655+Ankit-Dubey123@users.noreply.github.com>
2022-12-14 19:59:11 +0530

    Time: 5 ms (22.94%), Space: 7.8 MB (50.26%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        vector<int>temp(nums.size());
+        if(nums.size()==1)return nums[0];
+        temp[0]=nums[0];
+        temp[1]=max(nums[1],nums[0]);
+        for(int i=2;i<nums.size();i++){
+            temp[i]=max(temp[i-1],nums[i]+temp[i-2]);
+        }
+        return temp[nums.size()-1];
+    }
+};
