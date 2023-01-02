8bd17532e5a584c6aedb0287aada8c0235521dcc
Ankit Dubey <97017655+Ankit-Dubey123@users.noreply.github.com>
2023-01-02 20:31:44 +0530

    Time: 3 ms (87.02%), Space: 13.7 MB (43.90%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     void generate(int open,int closed,int n,string s,vector<string>&ans){
-        //base case
         if(open==n and closed==n){
             ans.push_back(s);
             return;
         }
-        
         if(open<n){
-            generate(open+1,closed,n,s+'(',ans);
+            generate(open+1,closed,n,s+'(',ans); // closed will increment only when (open>closed) 
         }
         if(closed<open){
             generate(open,closed+1,n,s+')',ans);
