40cf6cbfff4f6b9e0b3d2ded4df8c2c9cacb7ba2
Ankit Dubey <97017655+Ankit-Dubey123@users.noreply.github.com>
2024-04-25 02:41:41 +0530

    Time: 15 ms (68.47%), Space: 21.7 MB (12.00%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,10 +4,10 @@ public:
         int i=0;
         int j=1;
         while(j<nums.size()){
-            if(nums[i]==0 and nums[j]==0){
+            if(nums[i]==0 && nums[j]==0){
                 j++;
             }
-            else if(nums[i]==0 and nums[j]!=0){
+            else if(nums[i]==0 && nums[j]!=0){
                 swap(nums[i++],nums[j++]);
             }
             else{
