6f02a2ead66ef8459786257790c45bada3658e28
Ankit Dubey <97017655+Ankit-Dubey123@users.noreply.github.com>
2024-06-23 12:10:18 +0530

    Time: 3 ms (80.04%), Space: 16.4 MB (73.22%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int start =0 , end=nums.size()-1;
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(nums[mid]==target){
+        int start = 0, end = nums.size() - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
                 return true;
-            }
-            else if(nums[mid]==nums[start] && nums[mid]==nums[end]){
+            } else if (nums[mid] == nums[start] && nums[mid] == nums[end]) {
                 start++;
                 end--;
-            }
-            else if(nums[mid]>=nums[start]){
-                if(target>=nums[start] && target<=nums[mid]){
-                    end=mid-1;
+            } else if (nums[mid] >= nums[start]) {
+                if (target >= nums[start] && target <= nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
                 }
-                else{
-                    start=mid+1;
-                }
-            }
-            else{
-                if(target>=nums[mid] && target<=nums[end]){
-                    start = mid+1;
-                }
-                else{
-                    end=mid-1;
+            } else {
+                if (target >= nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
                 }
             }
         }
