e91130b1b4089c25f31eac1170a0b403aa2a25be
Ankit Dubey <97017655+Ankit-Dubey123@users.noreply.github.com>
2024-06-25 02:00:44 +0530

    Time: 43 ms (11.86%), Space: 21.4 MB (31.21%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -7,21 +7,19 @@ public:
         }
         return hours;
     }
-    int findMax(vector<int>&arr){
+    int findMax(vector<int>& arr) {
         int maxi = INT_MIN;
-        for(int i =0 ; i<arr.size();i++){
+        for (int i = 0; i < arr.size(); i++) {
             maxi = max(maxi, arr[i]);
         }
         return maxi;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
         int start = 1, end = findMax(piles);
-        //int ans = -1;
         while (start < end) {
             int mid = (start + end) / 2;
             int totalHour = func(piles, mid);
             if (totalHour <= h) {
-                //ans = mid;
                 end = mid;
             } else {
                 start = mid + 1;
