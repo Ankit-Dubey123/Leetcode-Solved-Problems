41c2a3ea34837b422c0323db108c1a21b4328268
Ankit Dubey <97017655+Ankit-Dubey123@users.noreply.github.com>
2024-08-03 12:32:52 +0530

    Time: 4 ms (38.91%), Space: 8.7 MB (47.03%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    void subset(int ind,vector<int>nums,vector<int>&v){
-        if(ind>=nums.size()){
-            ans.push_back(v);
-            return;
-        }
-        v.push_back(nums[ind]);
-        subset(ind+1,nums,v);
-        v.pop_back();
-        subset(ind+1,nums,v);
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int>v;
-        subset(0,nums,v);
+        vector<vector<int>> ans;
+        int size = nums.size();
+        for (int i = 0; i < pow(2,size); i++) {
+            vector<int> v;
+            int j = 0;
+            while(j<size){
+                if(i & (1<<j)){
+                    v.push_back(nums[j]);
+                }
+                j++;
+            }
+            ans.push_back(v);
+        }
         return ans;
     }
 };
