cb18faae42f552183f515630c25b1e8cf9bd7c98
Ankit Dubey <97017655+Ankit-Dubey123@users.noreply.github.com>
2024-08-27 00:42:43 +0530

    Time: 17 ms (47.69%), Space: 17.2 MB (69.23%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses);
+        for (int h = 0; h < prerequisites.size(); h++) {
+            graph[prerequisites[h][1]].push_back(prerequisites[h][0]);
+        }
+        vector<int> indegree(numCourses, 0);
+        for (int i = 0; i < numCourses; i++) {
+            for (auto it : graph[i]) {
+                indegree[it]++;
+            }
+        }
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        vector<int> topo;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for (auto it : graph[node]) {
+                indegree[it]--;
+                if (indegree[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+        if (topo.size() == numCourses) {
+            return true;
+        }
+        return false;
+    }
+};
