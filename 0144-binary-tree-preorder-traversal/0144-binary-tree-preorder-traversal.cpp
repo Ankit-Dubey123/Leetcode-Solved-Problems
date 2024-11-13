bcd07d10aa47d1240bd31070aef275df6dc6221d
Ankit Dubey <97017655+Ankit-Dubey123@users.noreply.github.com>
2024-11-13 23:58:25 +0530

    Time: 1 ms (6.4%), Space: 11.4 MB (9.33%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,17 +11,14 @@
  */
 class Solution {
 public:
-    void preOrder(TreeNode* root,vector<int>&ans){
-        if(root==NULL){
-            return;
-        }
-        ans.push_back(root->val);
-        preOrder(root->left,ans);
-        preOrder(root->right,ans);
-    }
+    vector<int> res;
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        preOrder(root,ans);
-        return ans;
+        if(root == NULL){
+            return {};
+        }
+        res.push_back(root->val);
+        preorderTraversal(root->left);
+        preorderTraversal(root->right);
+        return res;
     }
 };
