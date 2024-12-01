63fb933ef763049304f69146a6a6e72ac23f6ea6
Ankit Dubey <97017655+Ankit-Dubey123@users.noreply.github.com>
2024-12-01 22:49:33 +0530

    Time: 3 ms (13.26%), Space: 18.8 MB (5.02%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,40 +1,22 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int r = matrix.size();
-        int c = matrix[0].size();
-        int col0 = 1;
-        // mark first row and column
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
+        int row = matrix.size();
+        int col = matrix[0].size();
+        vector<int> rowVec(row, -1);
+        vector<int> colVec(col, -1);
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j<col; j++){
                 if(matrix[i][j]==0){
-                    matrix[i][0] = 0;
-                    if(j!=0){
-                        matrix[0][j] = 0;
-                    }
-                    else{
-                        col0 = 0; 
-                    }
+                    rowVec[i] = (matrix[i][j]==0) ? 1 : 0;
+                    colVec[j] = (matrix[i][j]==0) ? 1 : 0;
                 }
             }
         }
-        // Marks rest matrix with zeros
-        for(int i=1;i<r;i++){
-            for(int j=1;j<c;j++){
-                if(matrix[i][0]==0 or matrix[0][j]==0){
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-        // finally mark first row and column
-        if(matrix[0][0] == 0){
-            for(int j=0;j<c;j++){
-                matrix[0][j]=0;
-            }
-        }
-        if(col0 ==0){
-            for(int i=0;i<r;i++){
-                matrix[i][0] = 0;
+
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(rowVec[i] == 1 || colVec[j] == 1) matrix[i][j] = 0;
             }
         }
     }
