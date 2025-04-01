4ad4bad8423400881169b13fa0882b34b8665b5c
Ankit Dubey <97017655+Ankit-Dubey123@users.noreply.github.com>
2025-04-01 23:29:02 +0530

    Time: 0 ms (100%), Space: 9.7 MB (34.15%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> ans (numRows);
-        for(int i=0;i<numRows;i++){
-            ans[i].resize(i+1);
-            ans[i][0] = ans[i][i] = 1;
+        vector<vector<int>> res(numRows);
+        for(int i = 0 ;i<numRows;i++){
+            res[i].resize(i+1);
+            res[i][0] = res[i][i] = 1;
             for(int j = 1;j<i;j++){
-                ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
+                res[i][j] = res[i-1][j-1] + res[i-1][j];
             }
         }
-        return ans;
+        return res;
     }
 };
