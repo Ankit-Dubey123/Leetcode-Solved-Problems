78ae5f13f25400a7fe77d02bbcdca1602190b378
Ankit Dubey <97017655+Ankit-Dubey123@users.noreply.github.com>
2025-04-06 12:02:12 +0530

    Time: 0 ms (100%), Space: 10 MB (91.91%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,14 +3,15 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int r = matrix.size();
         int c = matrix[0].size();
-        // Swap [i][j] to [j][i]
+        // change matrix[i][j] to matrix[j][i], j will be increase from 0 to i
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < i; j++) {
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
-        // Reverse the matrix row
-        for (int i = 0; i <r;i++){
+
+        // Reverse all the rows
+        for (int i = 0; i < r; i++) {
             reverse(matrix[i].begin(), matrix[i].end());
         }
     }
