fcaf926f9e23f8394f9102fbceea217f44d71f07
Ankit Dubey <97017655+Ankit-Dubey123@users.noreply.github.com>
2026-03-16 00:16:24 +0530

    Time: 0 ms (100%), Space: 53.4 MB (80.99%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    let count0 = 0;
+    let count1 = 0;
+    let count2 = 0;
+
+    for (const num of nums){
+        if (num == 0){
+            count0++;
+        }
+        else if (num == 1){
+            count1++;
+        }
+        else if (num == 2){
+            count2++;
+        }
+    }
+    
+    let index = 0;
+    while(count0--){
+        nums[index++] = 0;
+    }
+    while(count1--){
+        nums[index++] = 1;
+    }
+    while(count2--){
+        nums[index++] = 2;
+    }
+};
